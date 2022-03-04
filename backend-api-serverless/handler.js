1979c0561e087eca0823b08ab0b839cff7790063
Diego´sGit <diego.martinezp@mail.udp.cl>
2022-03-04 15:56:59 -0300

    api-started-mockup-started

---
+++ backend-api-serverless/handler.js
@@ -0,0 +1,49 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+const mariadb = require('mariadb');
+
+// db settings 
+const pool = mariadb.createPool({
+  host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com", 
+  user:'bsale_test', 
+  password: 'bsale_test',
+  database:"bsale_test"
+});
+
+app.get("/getAllProducts", (req, res, next) => {
+  pool.getConnection()
+    .then(conn => {
+        console.log("conected!!");
+        conn.query("select * from product;")
+        .then(rows =>{
+          res.status(200).json({productos: rows})
+          return conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err);
+          return conn.end();
+        })
+        
+    }).catch(err => {
+      console.log("Not conected")
+      return res.status(503).json({
+        message: "db not connected",
+      });
+    });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
