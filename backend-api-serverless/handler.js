fb2c6c9f8b659bea0da27bcb8d73c84f536127dc
Diego´sGit <diego.martinezp@mail.udp.cl>
2022-03-08 15:56:26 -0300

    filter fixed and documentation v1.4.0

--- backend-api-serverless/handler.js
+++ backend-api-serverless/handler.js
@@ -1,100 +1,117 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const cors=require('cors');
-const dbConfig =require('./config');
+const cors = require("cors");
+const dbConfig = require("./config");
 
-
-// db settings 
+// db settings
 const pool = dbConfig;
-//corsOtions
-const corsOptions ={
-  "Access-Control-Allow-Origin": "http://127.0.0.1:5500*",
-  origin:'http://127.0.0.1:5500', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
+
+//corsOtions allow for test local and in the deploy
+const corsOptions = {
+  "Access-Control-Allow-Origin": [
+    "http://127.0.0.1:5500*",
+    "https://dev.d1t5l4r66dfh6a.amplifyapp.com*",
+  ],
+  origin: [
+    "http://127.0.0.1:5500",
+    "https://dev.d1t5l4r66dfh6a.amplifyapp.com",
+  ],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 //middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/');
-//   res.header('Access-Control-Allow-Credentials', true); 
-//   next();
-// });
+
+//route for get all products
 app.get("/getAllProducts", (req, res, next) => {
-  //db quering 
-  pool.getConnection()
-    .then(conn => {
-        console.log("conected!!");
-        conn.query("select * from product;")
-        .then(rows =>{
-          res.status(200).json({productos: rows})
+  //db quering
+  pool
+    .getConnection()
+    .then((conn) => {
+      console.log("conected!!");
+      conn
+        .query("select * from product;")
+        .then((rows) => {
+          //send status and products in json
+          res.status(200).json({ productos: rows });
           return conn.end();
         })
-        .catch(err => {
+        .catch((err) => {
           //handle error
           console.log(err);
-          return conn.end();
-        })
-        
-    }).catch(err => {
-      console.log("Not conected")
-      return res.status(503).json({
-        message: "db not connected",
-      });
+          conn.end();
+          return res.status(400).json({ message: "query error" });
+        });
+    })
+    .catch((err) => {
+      //handle error
+      console.log("Not conected");
+      return res.status(503).json({ message: "db not connected" });
     });
 });
 
+//get filters products with %like%
 app.post("/getProductsLike", (req, res, next) => {
-  //db quering 
-  pool.getConnection()
-    .then(conn => {
-        console.log("conected!!", req.body);
-        console.log(req.body.like)
-
-        conn.query("select * from product where name like '%" + req.body.like+"%';")
-        .then(rows =>{
-          res.status(200).json({productos: rows})
+  //db quering
+  pool
+    .getConnection()
+    .then((conn) => {
+      console.log("conected!!");
+      conn
+        .query(
+          "select * from product where name like '%" + req.body.like + "%';"
+        )
+        .then((rows) => {
+          //return status 200 and products %like%
+          res.status(200).json({ productos: rows });
           return conn.end();
         })
-        .catch(err => {
+        .catch((err) => {
           //handle error
           console.log(err);
-          return conn.end();
-        })
-        
-    }).catch(err => {
-      console.log("Not conected")
-      return res.status(503).json({
-        message: "db not connected",
-      });
+          conn.end();
+          return res.status(400).json({ message: "query error" });
+        });
+    })
+    .catch((err) => {
+      console.log("Not conected");
+      return res.status(503).json({ message: "db not connected" });
     });
 });
+
+//get All categories for navbar category menu
 app.get("/getAllCategories", (req, res, next) => {
-  //db quering 
-  pool.getConnection()
-    .then(conn => {
-        console.log("conected!!");
-        conn.query("select * from category;")
-        .then(rows =>{
-          res.status(200).json({categorias: rows})
+  //db quering
+  pool
+    .getConnection()
+    .then((conn) => {
+      console.log("conected!!");
+      conn
+        .query("select * from category;")
+        .then((rows) => {
+          //return categories and status 200 all ok
+          res.status(200).json({ categorias: rows });
           return conn.end();
         })
-        .catch(err => {
+        .catch((err) => {
           //handle error
           console.log(err);
-          return conn.end();
-        })
-        
-    }).catch(err => {
-      console.log("Not conected")
+          conn.end();
+          return res.status(400).json({ message: "query error" });
+        });
+    })
+    .catch((err) => {
+      console.log("Not conected");
       return res.status(503).json({
         message: "db not connected",
       });
     });
 });
 
+//in case of conection refuse
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
--- frontend/src/productos.js
+++ frontend/src/productos.js
@@ -1,84 +1,105 @@
+const lAMBDA_URL = "https://pfcetve728.execute-api.us-east-1.amazonaws.com/"; //lambda endpoint
+const contenedorProductos = document.getElementById("contenedorProductos"); //products contanter
+const contenedorCategorias = document.getElementById("contenedorCat"); //categories contanter
+let categoriasDropdown = ""; //listed categories
+let productosGrid = ""; //listed products
 
-const lAMBDA_URL='https://pfcetve728.execute-api.us-east-1.amazonaws.com/'
-const contenedorProductos=document.getElementById("contenedorProductos")
-const contenedorCategorias=document.getElementById("contenedorCat")
-let categoriasDropdown=''
-let productosGrid=''
-
-const mostrar= (productos)=>{
-    
-    productos.forEach(producto => {
-        productosGrid+= `<div class="container-fluid col"> 
+//function to show products that receive in array
+const mostrar = (productos) => {
+  productos.forEach((producto) => {
+    //add html card for each product
+    productosGrid += `<div class="container-fluid col"> 
                             <div class="card mt-3 mb-3" style="width: 300px;">
-                                <img src="${producto.url_image}" class="card-img-top" width="300" height="300"alt="  Upps error en carga"/>
+                                <img src="${
+                                  producto.url_image
+                                }" class="card-img-top" width="300" height="300"alt="  Upps error en carga"/>
                                 <div class="card-body"style="height: 160px;">
                                 <h5 class="card-title">${producto.name}</h5>
                                 <p class="card-text">Precio: $ ${producto.price}
-                                ${producto.discount !==0? `<span class="discount">-${producto.discount}</span>`:``}
+                                ${
+                                  producto.discount !== 0
+                                    ? `<span class="discount">-${producto.discount}</span>`
+                                    : ``
+                                }
                                 </p>
                                 <a href="#" class="btn btn-dark">add to cart</a>
                                 </div>
                             </div>
-                        </div>`
-    });
-    contenedorProductos.innerHTML=productosGrid
-}
-const mostrarCat= (categorias)=>{
-    categorias.forEach(categoria => {
-        categoriasDropdown+= `<li onclick="filterByCategory(event,this.id)" id="${categoria.id}"><a class="dropdown-item" href="#">${categoria.name}</a></li>`
-    });
-    contenedorCategorias.innerHTML=categoriasDropdown
-}
+                        </div>`;
+  });
+  //actualize the products in frontend
+  contenedorProductos.innerHTML = productosGrid;
+};
+//function to show categories in navbar category filter
+const mostrarCat = (categorias) => {
+  categorias.forEach((categoria) => {
+    //add html <li> with category
+    categoriasDropdown += `<li onclick="filterByCategory(event,this.id)" id="${categoria.id}"><a class="dropdown-item" href="#">${categoria.name}</a></li>`;
+  });
+  //actualize categories in dropdown menu
+  contenedorCategorias.innerHTML = categoriasDropdown;
+};
 
-const getProducts=async()=> await fetch( lAMBDA_URL+'getAllProducts')
-.then(async res => {
-const data= await res.json();
-productosMostrados=data.productos
-mostrar(data.productos)
-console.log(data.productos)
-})
-.catch(error => console.error(error))
+//function to fetch all products from lambda function in backend
+const getProducts = async () =>
+  await fetch(lAMBDA_URL + "getAllProducts")
+    .then(async (res) => {
+      //parse the response to json
+      const data = await res.json();
+      //set value of "Productos mostrados to all products"
+      productosMostrados = data.productos;
+      mostrar(data.productos);
+    })
+    .catch((error) => console.error(error));
 getProducts();
 
-
-const getCategories=async()=> await fetch( lAMBDA_URL+'getAllCategories')
-.then(async res => {
-let data= await res.json();
-
-mostrarCat(data.categorias)
-console.log(data.categorias)
-})
-.catch(error => console.error(error))
+//function to get all categories drom lambda function in backend
+const getCategories = async () =>
+  await fetch(lAMBDA_URL + "getAllCategories")
+    .then(async (res) => {
+      //parse the response to json
+      let data = await res.json();
+      mostrarCat(data.categorias);
+    })
+    .catch((error) => console.error(error));
 getCategories();
 
-async function productsLike(event){
-    event.preventDefault();
-    let search=document.getElementById("search").value;
-    const requestOptions={
-        method: 'POST',
-        credentials: "include",
-        origin: true,
-        mode: 'cors',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({like: search})
-    };
-    console.log("buscando",search)
-    
-    await fetch( lAMBDA_URL+'getProductsLike',requestOptions)
-    .then(async res => {
-    data= await res.json();
-    productosGrid=''
-    productosMostrados=data.productos
-    console.log(data.productos)
-    mostrar(data.productos)
+//function to get products filter with "search" value from lambda function in backend
+async function productsLike(event) {
+  //prevent recharge page
+  event.preventDefault();
+  //select filter value in input
+  let search = document.getElementById("search").value;
+  //request options
+  const requestOptions = {
+    method: "POST",
+    credentials: "include",
+    origin: true,
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ like: search }),
+  };
+  //fetch prducts with finter
+  await fetch(lAMBDA_URL + "getProductsLike", requestOptions)
+    .then(async (res) => {
+      //parse resquest data to json
+      data = await res.json();
+      //restart products in grid
+      productosGrid = "";
+      //set value of "Productos mostrados to filter products"
+      productosMostrados = data.productos;
+      mostrar(data.productos);
     })
-    .catch(error =>{
-        console.error(error) 
-    } )
+    .catch((error) => {
+      console.error(error);
+    });
 }
-
- const filterByCategory=(event,id)=>{
-    event.preventDefault();
-    productosGrid=''
-    mostrar(productosMostrados.filter(producto=>producto.category==id))
-}
+//funtion to filter by category from navbar category dropdown-menu
+const filterByCategory = (event, id) => {
+  //prevent recharge page
+  event.preventDefault();
+  //reset products grid
+  productosGrid = "";
+  //call mostrar with filtered by category products
+  mostrar(productosMostrados.filter((producto) => producto.category == id));
+};
